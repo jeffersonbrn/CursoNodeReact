8d2873f1fc0680968549624d4368dc517d6004cc
jeffersonbrn <jeffersonbrn@live.com>
2021-04-22 22:02:39 -0300

    Complete the create the frontend

--- server/index.js
+++ server/index.js
@@ -1,8 +1,21 @@
-const express = require('express')
-const app = express()
-const port = 3001
+const express = require('express');
+const app = express();
+const cors = require('cors')
+const port = 3001;
 
-app.listen(port, () => {
-    console.log("Server running on port " + port)
+app.use(express.json());
+app.use(cors());
+
+const db = require('./models');
+
+//Routes
+const postRouter = require('./routes/Posts');
+app.use("/posts", postRouter)
+
+db.sequelize.sync().then(() => {
+    app.listen(port, () => {
+        console.log("Server running on port " + port)
+    });    
 });
+
 
