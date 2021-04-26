7d2689bbfb26d749eb1dfa7ffdbb5622fc142534
Bruno Reis <jeffersonbrn@live.com>
2021-04-26 13:29:45 -0300

    Concluindo post de dados

--- client/src/pages/CreatePost.js
+++ client/src/pages/CreatePost.js
@@ -1,16 +1,60 @@
 import React from "react";
 import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import axios from "axios";
 
 function CreatePost() {
+  const initialValues = {
+    title: "",
+    postText: "",
+    username: "",
+  };
+
+  const validationSchema = Yup.object().shape({
+    title: Yup.string().required("Necessário um título"),
+    postText: Yup.string().required(),
+    username: Yup.string().min(3).max(15).required(),
+  });
+
+  const onSubmit = (data) => {
+    axios.post("http://localhost:3001/posts", data).then((response) => {
+      console.log("It Worked");
+    });
+  };
+
   return (
     <div className="createPostPage">
-      <Formik initialValues={1} onSubmit={1} validationSchema={1}>
-        <Form>
+      <Formik
+        initialValues={initialValues}
+        onSubmit={onSubmit}
+        validationSchema={validationSchema}
+      >
+        <Form className="formContainer">
+          <label>Title: </label>
+          <ErrorMessage name="title" component="span" />
           <Field
+            autocomplete="off"
             id="inputCreatePost"
             name="title"
             placeholder="(Ex. Bruno..."
           />
+          <label>Post: </label>
+          <ErrorMessage name="postText" component="span" />
+          <Field
+            autocomplete="off"
+            id="inputCreatePost"
+            name="postText"
+            placeholder="(Ex. Post..."
+          />
+          <label>Username: </label>
+          <ErrorMessage name="username" component="span" />
+          <Field
+            autocomplete="off"
+            id="inputCreatePost"
+            name="username"
+            placeholder="(Ex. Username..."
+          />
+          <button type="submit">Create Post</button>
         </Form>
       </Formik>
     </div>
