fe6f08ecb11a76aa04add875161b827968465a3c
Bruno Reis <jeffersonbrn@live.com>
2021-04-27 13:33:13 -0300

    Criando pagina de gerenciamento de post

--- client/src/App.js
+++ client/src/App.js
@@ -2,16 +2,20 @@ import "./App.css";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import CreatePost from "./pages/CreatePost";
+import Post from "./pages/Post";
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Link to="/CreatePost">Create a Post</Link>
-        <Link to="/">Go back home</Link>
+        <div className="navbar">
+          <Link to="/">Go back home</Link>
+          <Link to="/CreatePost">Create a Post</Link>
+        </div>
         <Switch>
           <Route path="/" exact component={Home} />
           <Route path="/CreatePost" exact component={CreatePost} />
+          <Route path="/post/:id" exact component={Post} />
         </Switch>
       </Router>
     </div>
